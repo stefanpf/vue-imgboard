af7d10f116eb93bc44b9d829f872abaf698ad4b6
Stefan Pfeuffer <mail@stefanpfeuffer.com>
2022-01-04 11:49:41 +0100

    basic file upload functionality

--- public/js/app.js
+++ public/js/app.js
@@ -5,14 +5,39 @@ Vue.createApp({
         return {
             heading: "Latest Images",
             images: [],
+            title: "",
+            description: "",
+            file: null,
+            username: "",
         };
     },
     mounted() {
-        fetch("/get-image-urls")
+        fetch("/get-images")
             .then((res) => res.json())
             .then((data) => {
                 this.images = data;
             })
             .catch((err) => console.log("Err in mounted:", err));
+    },
+    methods: {
+        clickHandler: function () {
+            const fd = new FormData();
+            fd.append("title", this.title);
+            fd.append("description", this.description);
+            fd.append("username", this.username);
+            fd.append("file", this.file);
+            fetch("/upload", {
+                method: "POST",
+                body: fd,
+            })
+                .then((res) => res.json())
+                .then(console.log)
+                .catch((err) => {
+                    console.log("Err in image upload:", err);
+                });
+        },
+        fileSelectHandler: function (e) {
+            this.file = e.target.files[0];
+        },
     },
 }).mount("#main");
--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const { logUrl } = require("./utils/helper-functions");
 const db = require("./utils/db");
+const { uploader } = require("./utils/upload");
 const app = express();
+const PORT = 8080;
 
 app.use(logUrl);
 app.use(express.static("./public"));
 app.use(express.json());
 
-app.get("/get-image-urls", (req, res) => {
+app.post("/upload", uploader.single("file"), (req, res) => {
+    if (req.file) {
+        res.json({ success: true });
+    } else {
+        res.json({ success: false });
+    }
+});
+
+app.get("/get-images", (req, res) => {
     db.getImages()
         .then(({ rows }) => {
             res.json(rows);
@@ -21,4 +31,6 @@ app.get("*", (req, res) => {
     res.sendFile(`${__dirname}/index.html`);
 });
 
-app.listen(8080, () => console.log(`I'm listening.`));
+app.listen(PORT, () =>
+    console.log(`Imageboard server listening on port ${PORT}`)
+);
